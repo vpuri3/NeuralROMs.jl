a01a891511cd4fbc9a68a5bf44b3dda26ca486c1
Vedant Puri <vedantpuri@gmail.com>
2023-08-02 07:48:02 -0400

    update run scripts

--- examples/diffusion_fourier/exp_2d/model.jl
+++ examples/diffusion_fourier/exp_2d/model.jl
@@ -47,33 +47,35 @@ include("../datagen.jl")
 BSON.@load joinpath(@__DIR__, "..", "data2D_N$(N).bson") _data data_
 
 rng = Random.default_rng()
-Random.seed!(rng, 345)
+Random.seed!(rng, 123)
 
-__data = combine_data2D(_data)
-data__ = combine_data2D(data_)
+__data = combine_data2D(_data, 128)
+data__ = combine_data2D(data_, 128)
 
 ###
 # FNO model
 ###
-if true
+if false
 
-w = 12        # width
-m = (12, 12,) # modes
+w = 16        # width
+m = (16, 16,) # modes
 c = size(__data[1], 1) # in  channels
 o = size(__data[2], 1) # out channels
 
 NN = Lux.Chain(
-    Dense(c , w, tanh),
+    PermutedBatchNorm(c, 4),
+    Dense(c, w, tanh),
     OpKernel(w, w, m, tanh),
     OpKernel(w, w, m, tanh),
     OpKernel(w, w, m, tanh),
-    Dense(w , o)
+    OpKernel(w, w, m, tanh),
+    Dense(w, o),
 )
 
 opt = Optimisers.Adam()
 batchsize = 128
 learning_rates = (1f-2, 1f-3,)
-nepochs  = E .* (0.10, 0.90,) .|> Int
+nepochs  = E .* (0.20, 0.80,) .|> Int
 dir = joinpath(@__DIR__, "exp_FNO_nonlin")
 device = Lux.gpu
 
@@ -86,18 +88,18 @@ end
 # Bilinear (linear / nonlin) model
 ###
 
-if false
+if true
 
 # fixed params
 c1 = 3     # in  channel nonlin
 c2 = 1     # in  channel linear
-o  = size(__data[2]   , 1) # out channel
+o  = size(__data[2], 1) # out channel
 
 # hyper params
-w1 = 8       # width nonlin
-w2 = 8       # width linear
-wo = 8        # width project
-m = (12, 12,) # modes
+w1 = 32       # width nonlin
+w2 = 32       # width linear
+wo = 1        # width project
+m = (16, 16,) # modes
 
 split = SplitRows(1:3, 4)
 nonlin = Chain(
@@ -107,26 +109,26 @@ nonlin = Chain(
     )
 linear = Chain(
         Dense(c2, w2, use_bias = false),
-        OpConv(w2, w2, m)
     )
 bilin  = OpConvBilinear(w1, w2, wo, m)
 # bilin  = OpKernelBilinear(w1, w2, o, m) # errors
 
 project = Dense(wo, o, use_bias = false)
+project = NoOpLayer()
 
 NN = linear_nonlinear(split, nonlin, linear, bilin, project)
 
 opt = Optimisers.Adam()
 batchsize = 128
-learning_rates = (1f-3,)
+learning_rates = (1f-5,)
 nepochs  = E .* (1.00,) .|> Int
 dir = joinpath(@__DIR__, "exp_FNO_linear_nonlinear")
 device = Lux.gpu
 
-FNO_bl, _ = train_model(rng, NN, __data, data__, V, opt;
+FNO_bl, ST = train_model(rng, NN, __data, data__, V, opt;
         batchsize, learning_rates, nepochs, dir, device)
 
-end
+plt = plot_training(ST...)
 
-nothing
+end
 #
