e05682c39b612bb869d61fc68daa6c1fc10b1f37
Vedant Puri <vedantpuri@gmail.com>
2024-01-28 18:29:30 -0500

    misc

--- misc.jl
+++ misc.jl
@@ -27,6 +27,7 @@
 ################
 # fourth order derivative
 ################
+#
 # using ForwardDiff
 # using ForwardDiff: Dual, value, partials
 #
@@ -57,3 +58,47 @@
 #
 # fx, d1f, d2f, d3f, d4f
 ################
+# Split Decoder
+################
+#
+# pou = begin
+#     in_layer = Dense(1, w, act; init_weight = init_wt_in, init_bias)
+#     hd_layer = Dense(w, w, act; init_weight = init_wt_hd, init_bias)
+#     fn_layer = Dense(w, 1; init_weight = init_wt_fn, init_bias, use_bias = use_bias_fn)
+#
+#     # in_layer = Dense(l+1, w, sin)
+#     # hd_layer = Dense(w  , w, sin)
+#     # fn_layer = Dense(w  , 1)
+#
+#     Chain(
+#         in_layer,
+#         fill(hd_layer, h)...,
+#         fn_layer,
+#     )
+# end
+#
+# coef = begin
+#     act2 = tanh
+#     act2 = elu
+#
+#     vec_layer = WrappedFunction(vec)
+#     embedding = Embedding(1 => l)
+#
+#     in_layer = Dense(l, w, act2)
+#     hd_layer = Dense(w, w, act2)
+#     fn_layer = Dense(w, 1)
+#
+#     Chain(
+#         vec_layer,
+#         embedding,
+#         in_layer,
+#         fill(hd_layer, h)...,
+#         fn_layer,
+#         softmax,
+#     )
+# end
+#
+# connection = (x -> sum(x; dims = 1)) ∘ .*
+# NN = Parallel(connection, pou, coef) # (x -> NN1) .* (ũ -> NN2)
+################
+    
