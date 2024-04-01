c41c6ad395d0de04db110b19bcd992e492aa1eff
Vedant Puri <vedantpuri@gmail.com>
2024-04-01 14:52:16 -0400

    ablation plot

--- examples/ks_fourier1D/ks1d_compare.jl
+++ examples/ks_fourier1D/ks1d_compare.jl
@@ -29,6 +29,7 @@ modeldir_PCA = joinpath(@__DIR__, "model_PCA$(lp)") # traditional
 modeldir_CAE = joinpath(@__DIR__, "model_CAE$(l0)") # Lee, Carlberg
 modeldir_SNW = joinpath(@__DIR__, "model_SNW$(l0)") # us (Weight decay)
 modeldir_SNL = joinpath(@__DIR__, "model_SNL$(l0)") # us (Lipschitz)
+modeldir_SN0 = joinpath(@__DIR__, "model_SN0$(l0)") # us (Lipschitz)
 
 # # train PCA
 # train_PCA(datafile, modeldir_PCA, l_pca; rng, makedata_kws, device)
@@ -41,9 +42,13 @@ modeldir_SNL = joinpath(@__DIR__, "model_SNL$(l0)") # us (Lipschitz)
 # train_params_SNW = (; E = 1400, wd = 128, α = 0f-0, γ = 1f-2, makedata_kws,)
 # train_SNF_compare(latent, datafile, modeldir_SNW, train_params_SNW; rng, device)
 #
-# train_SNL
+# # train_SNL
 # train_params_SNL = (; E = 1400, wd = 128, wh = 8, hh = 0, α = 1f-7, γ = 0f-0, makedata_kws,)
 # train_SNF_compare(latent, datafile, modeldir_SNL, train_params_SNL; rng, device)
+#
+# # train_SNL
+# train_params_SN0 = (; E = 1400, wd = 128, wh = 8, hh = 0, makedata_kws,)
+# train_SNF_compare(latent, datafile, modeldir_SN0, train_params_SN0; rng, device)
 
 #==================#
 # postprocess
@@ -53,19 +58,21 @@ modelfile_PCA = joinpath(modeldir_PCA, "model.jld2")
 modelfile_CAE = joinpath(modeldir_CAE, "model_07.jld2")
 modelfile_SNW = joinpath(modeldir_SNW, "model_08.jld2")
 modelfile_SNL = joinpath(modeldir_SNL, "model_08.jld2")
+modelfile_SN0 = joinpath(modeldir_SN0, "model_08.jld2")
 
-postprocess_PCA(prob, datafile, modelfile_PCA; rng, device)
-postprocess_CAE(prob, datafile, modelfile_CAE; rng)
-postprocess_SNF(prob, datafile, modelfile_SNW; rng, device)
-postprocess_SNF(prob, datafile, modelfile_SNL; rng, device)
+# postprocess_PCA(prob, datafile, modelfile_PCA; rng, device)
+# postprocess_CAE(prob, datafile, modelfile_CAE; rng)
+# postprocess_SNF(prob, datafile, modelfile_SNW; rng, device)
+# postprocess_SNF(prob, datafile, modelfile_SNL; rng, device)
+# postprocess_SNF(prob, datafile, modelfile_SN0; rng, device)
 
 #==================#
 # make figures
 #==================#
 grid = (256,)
 casename = "ks1d"
-modeldirs = (; modeldir_PCA, modeldir_CAE, modeldir_SNW, modeldir_SNL,)
-labels = ("POD ($(l_pca) modes)", "CAE", "SNFW (ours)", "SNFL (ours)")
+modeldirs = (; modeldir_PCA, modeldir_CAE, modeldir_SNL, modeldir_SNW) #, modeldir_SN0)
+labels = ("POD ($(l_pca) modes)", "CAE", "SNFL (ours)", "SNFW (ours)") #, "Ablation")
 
 p1, p2, p3 = compare_plots(modeldirs, labels, @__DIR__, casename, 1, grid)
 
