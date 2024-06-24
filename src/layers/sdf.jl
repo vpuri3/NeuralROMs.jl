75a792755d12722cb1bb3cad19123c6b14959464
Vedant Puri <vedantpuri@gmail.com>
2024-06-24 18:34:55 -0400

    reorganize MLH and precompute indices

--- src/layers/sdf.jl
+++ src/layers/sdf.jl
@@ -1,37 +1,37 @@
 #
 #===========================================================#
-clamp_vanilla(x::AbstractArray, δ) = @. clamp(x, -δ, δ)
-clamp_tanh(x::AbstractArray, δ)    = @. δ * tanh_fast(x)
-clamp_sigmoid(x::AbstractArray, δ) = @. δ * (2 * sigmoid_fast(x) - 1)
-clamp_softsign(x::AbstractArray,δ) = @. δ * softsign(x)
+clamp_tanh(x::AbstractArray    , δ) = @. δ * tanh_fast(x)
+clamp_vanilla(x::AbstractArray , δ) = @. clamp(x, -δ, δ)
+clamp_sigmoid(x::AbstractArray , δ) = @. δ * (2 * sigmoid_fast(x) - 1)
+clamp_softsign(x::AbstractArray, δ) = @. δ * softsign(x)
 
-struct ClampVanilla{T <: Real}  <: Lux.AbstractExplicitLayer; δ::T; end
 struct ClampTanh{T <: Real}     <: Lux.AbstractExplicitLayer; δ::T; end
+struct ClampVanilla{T <: Real}  <: Lux.AbstractExplicitLayer; δ::T; end
 struct ClampSigmoid{T <: Real}  <: Lux.AbstractExplicitLayer; δ::T; end
 struct ClampSoftsign{T <: Real} <: Lux.AbstractExplicitLayer; δ::T; end
 
-Lux.initialstates(::ClampVanilla ) = (;)
 Lux.initialstates(::ClampTanh    ) = (;)
+Lux.initialstates(::ClampVanilla ) = (;)
 Lux.initialstates(::ClampSigmoid ) = (;)
 Lux.initialstates(::ClampSoftsign) = (;)
 
-Lux.initialparameters(::ClampVanilla ) = (;)
 Lux.initialparameters(::ClampTanh    ) = (;)
+Lux.initialparameters(::ClampVanilla ) = (;)
 Lux.initialparameters(::ClampSigmoid ) = (;)
 Lux.initialparameters(::ClampSoftsign) = (;)
 
-Lux.statelength(::ClampVanilla ) = 0
 Lux.statelength(::ClampTanh    ) = 0
+Lux.statelength(::ClampVanilla ) = 0
 Lux.statelength(::ClampSigmoid ) = 0
 Lux.statelength(::ClampSoftsign) = 0
 
-Lux.parameterlength(::ClampVanilla ) = 0
 Lux.parameterlength(::ClampTanh    ) = 0
+Lux.parameterlength(::ClampVanilla ) = 0
 Lux.parameterlength(::ClampSigmoid ) = 0
 Lux.parameterlength(::ClampSoftsign) = 0
 
-(l::ClampVanilla )(x::AbstractArray, p, st) = clamp_vanilla(x, l.δ), st
 (l::ClampTanh    )(x::AbstractArray, p, st) = clamp_tanh(x, l.δ), st
+(l::ClampVanilla )(x::AbstractArray, p, st) = clamp_vanilla(x, l.δ), st
 (l::ClampSigmoid )(x::AbstractArray, p, st) = clamp_sigmoid(x, l.δ), st
 (l::ClampSoftsign)(x::AbstractArray, p, st) = clamp_softsign(x, l.δ), st
 
@@ -65,10 +65,37 @@ end
 
 function get_boundingbox(
     xyz::AbstractArray{T},
-    shape::NTuple{D, Ti},
+    shape::NTuple{2, Ti},
     nEmbeddings,
     indexfun,
-) where{T, D, Ti}
+) where{T, Ti}
+
+    @assert size(xyz, 1) == 2
+
+    Ixyz0, Ixyz1, wxyz = get_boundingbox_indices(xyz, shape)
+
+    Ix0, Iy0 = collect(getindex(Ixyz0, d, :) for d in axes(Ixyz0, 1))
+    Ix1, Iy1 = collect(getindex(Ixyz1, d, :) for d in axes(Ixyz1, 1))
+    wx , wy  = collect(getindex(wxyz , d, :) for d in axes(wxyz , 1))
+
+    wx , wy  = map(w -> reshape(w, 1, size(w)...), (wx, wy))
+
+    I00 = indexfun(Ix0, Iy0, nEmbeddings, shape)
+    I10 = indexfun(Ix1, Iy0, nEmbeddings, shape)
+    I01 = indexfun(Ix0, Iy1, nEmbeddings, shape)
+    I11 = indexfun(Ix1, Iy1, nEmbeddings, shape)
+
+    (wx, wy), (I00, I10, I01, I11,)
+end
+
+function get_boundingbox(
+    xyz::AbstractArray{T},
+    shape::NTuple{3, Ti},
+    nEmbeddings,
+    indexfun,
+) where{T, Ti}
+
+    @assert size(xyz, 1) == 3
 
     Ixyz0, Ixyz1, wxyz = get_boundingbox_indices(xyz, shape)
 
@@ -78,26 +105,41 @@ function get_boundingbox(
 
     wx , wy , wz  = map(w -> reshape(w, 1, size(w)...), (wx, wy, wz))
 
-    I000 = indexfun(Ix0, Iy0, Iz0, nEmbeddings)
-    I100 = indexfun(Ix1, Iy0, Iz0, nEmbeddings)
-    I010 = indexfun(Ix0, Iy1, Iz0, nEmbeddings)
-    I110 = indexfun(Ix1, Iy1, Iz0, nEmbeddings)
-    I001 = indexfun(Ix0, Iy0, Iz1, nEmbeddings)
-    I101 = indexfun(Ix1, Iy0, Iz1, nEmbeddings)
-    I011 = indexfun(Ix0, Iy1, Iz1, nEmbeddings)
-    I111 = indexfun(Ix1, Iy1, Iz1, nEmbeddings)
+    I000 = indexfun(Ix0, Iy0, Iz0, nEmbeddings, shape)
+    I100 = indexfun(Ix1, Iy0, Iz0, nEmbeddings, shape)
+    I010 = indexfun(Ix0, Iy1, Iz0, nEmbeddings, shape)
+    I110 = indexfun(Ix1, Iy1, Iz0, nEmbeddings, shape)
+    I001 = indexfun(Ix0, Iy0, Iz1, nEmbeddings, shape)
+    I101 = indexfun(Ix1, Iy0, Iz1, nEmbeddings, shape)
+    I011 = indexfun(Ix0, Iy1, Iz1, nEmbeddings, shape)
+    I111 = indexfun(Ix1, Iy1, Iz1, nEmbeddings, shape)
 
-    (wx, wy, wz), (I000, I100, I010, I110, I001, I101, I011, I111)
+    (wx, wy, wz), (I000, I100, I010, I110, I001, I101, I011, I111,)
 end
 
 #===========================================================#
 # index schemes
 #===========================================================#
+
+function index_hash(
+    Ix::AbstractArray{Ti},
+    Iy::AbstractArray{Ti},
+    nEmbeddings::Integer,
+    shape::NTuple{2, <:Integer},
+) where{Ti <: Integer}
+
+    # https://bigprimes.org/
+    primes = Int32[1, 481549]
+    val = xor.(Ix * primes[1], Iy * primes[2])
+    mod1.(val, Ti(nEmbeddings))
+end
+
 function index_hash(
     Ix::AbstractArray{Ti},
     Iy::AbstractArray{Ti},
     Iz::AbstractArray{Ti},
     nEmbeddings::Integer,
+    shape::NTuple{3, <:Integer},
 ) where{Ti <: Integer}
 
     # https://bigprimes.org/
@@ -109,20 +151,33 @@ end
 function index_grid(
     Ix::AbstractArray{Ti},
     Iy::AbstractArray{Ti},
+    nEmbeddings::Integer,
+    shape::NTuple{2, <:Integer},
+) where{Ti}
+    @. (Iy - 1) * shape[1] + Ix
+end
+
+function index_grid(
+    Ix::AbstractArray{Ti},
+    Iy::AbstractArray{Ti},
     Iz::AbstractArray{Ti},
+    nEmbeddings::Integer,
     shape::NTuple{3, <:Integer},
 ) where{Ti}
     @. (Iz - 1) * (shape[1] * shape[2]) + (Iy - 1) * shape[1] + Ix
 end
 
 #===========================================================#
-# Spatial hash grid
+# Abstract feature grid types
 #===========================================================#
 
-@concrete struct SpatialHash{D} <: Lux.AbstractExplicitContainerLayer{(:embedding,)} where{D}
+export SpatialHash, SpatialGrid
+
+@concrete struct FeatureGrid{D} <: Lux.AbstractExplicitContainerLayer{(:embedding,)}
     shape
-    embedding
+    indexfun
     interpfun
+    embedding
 end
 
 function SpatialHash(
@@ -135,17 +190,46 @@ function SpatialHash(
 
     embedding = Embedding(nEmbeddings => out_dims; init_weight)
 
-    SpatialHash{D}(shape, embedding, interpfun)
+    FeatureGrid{D}(shape, index_hash, interpfun, embedding)
 end
 
-function (l::SpatialHash)(xyz::AbstractArray{T}, p, st) where{T}
+function SpatialGrid(
+    out_dims::Integer,
+    shape::NTuple{D, Integer};
+    init_weight = randn32,
+    interpfun = lerp,
+) where{D}
+
+    nEmbeddings = prod(shape)
+    embedding = Embedding(nEmbeddings => out_dims; init_weight)
+
+    FeatureGrid{D}(shape, index_grid, interpfun, embedding)
+end
+
+function (l::FeatureGrid)(xyz::AbstractArray, p, st)
     @assert size(xyz, 1) == length(l.shape)
-    wxyz, Ibbox = Zygote.@ignore get_boundingbox(xyz, l.shape, l.embedding.in_dims, index_hash)
+    wxyz, Ibbox = Zygote.@ignore get_boundingbox(xyz, l.shape, l.embedding.in_dims, l.indexfun)
 
     l((wxyz, Ibbox), p, st)
 end
 
-function (l::SpatialHash)((wxyz, Ibbox), p, st)
+function (l::FeatureGrid{2})((wxyz, Ibbox)::Tuple, p, st)
+    wx, wy = wxyz
+    I00, I10, I01, I11 = Ibbox
+
+    f00, _ = l.embedding(I00, p, st)
+    f10, _ = l.embedding(I10, p, st)
+    f01, _ = l.embedding(I01, p, st)
+    f11, _ = l.embedding(I11, p, st)
+
+    fx0 = l.interpfun(f00, f10, wx) # y = 0
+    fx1 = l.interpfun(f01, f11, wx) # y = 1
+    f   = l.interpfun(fx0, fx1, wy) # interp
+
+    f, st
+end
+
+function (l::FeatureGrid{3})((wxyz, Ibbox)::Tuple, p, st)
     wx, wy, wz = wxyz
     I000, I100, I010, I110, I001, I101, I011, I111 = Ibbox
 
@@ -174,65 +258,8 @@ function (l::SpatialHash)((wxyz, Ibbox), p, st)
 end
 
 #===========================================================#
-# Spatial grid
-#===========================================================#
 
-# @concrete struct FeatureGrid{D} <: Lux.AbstractExplicitContainerLayer{(:embedding,)} where{D}
-#     shape
-#     embedding
-#     interpfun
-# end
-#
-# function FeatureGrid(
-#     out_dims::Integer,
-#     shape::NTuple{D, Integer};
-#     init_weight = randn32,
-#     interpfun = lerp,
-# ) where{D}
-#
-#     nEmbeddings = prod(shape)
-#     embedding = Embedding(nEmbeddings => out_dims; init_weight)
-#
-#     FeatureGrid{D}(shape, embedding, interpfun)
-# end
-#
-# function (l::FeatureGrid)(xyz::AbstractArray{T}, p, st) where{T}
-#     @assert size(xyz, 1) == length(l.shape)
-#     wxyz, Ibbox = Zygote.@ignore get_boundingbox(xyz, l.shape, l.embedding.in_dims, index_grid)
-#
-#     l((wxyz, Ibbox), p, st)
-# end
-#
-# function (l::FeatureGrid)((wxyz, Ibbox), p, st)
-#     wx, wy, wz = Zygote.@ignore wxyz
-#     I000, I100, I010, I110, I001, I101, I011, I111 = Zygote.@ignore Ibbox
-#
-#     f000, _ = l.embedding(I000, p, st)
-#     f100, _ = l.embedding(I100, p, st)
-#     f010, _ = l.embedding(I010, p, st)
-#     f110, _ = l.embedding(I110, p, st)
-#     f001, _ = l.embedding(I001, p, st)
-#     f101, _ = l.embedding(I101, p, st)
-#     f011, _ = l.embedding(I011, p, st)
-#     f111, _ = l.embedding(I111, p, st)
-#
-#     # bottom (z=0)
-#     fx00 = l.interpfun(f000, f100, wx)
-#     fx10 = l.interpfun(f010, f110, wx)
-#     fxy0 = l.interpfun(fx00, fx10, wy)
-#
-#     # top (z=1)
-#     fx01 = l.interpfun(f001, f101, wx)
-#     fx11 = l.interpfun(f011, f111, wx)
-#     fxy1 = l.interpfun(fx01, fx11, wy)
-#
-#     f = l.interpfun(fxy0, fxy1, wz)
-#
-#     f, st
-# end
-#===========================================================#
-
-export MultiLevelSpatialHash
+export MultiLevelSpatialHash, precompute_MLH
 function MultiLevelSpatialHash(;
     out_dims::Integer = 2,
     nEmbeddings::Integer = 2^14,
@@ -248,40 +275,42 @@ function MultiLevelSpatialHash(;
 
     println("Generating $(nLevels) spatial hashes with resolutions $(Ns)")
 
-    # levels = (;)
-    # for l in 1:nLevels
-    #     N = Ns[l]
-    #     shape = (N, N, N,)
-    #     Ne = min(prod(shape), nEmbeddings)
-    #     layer = SpatialHash(out_dims, nEmbeddings)
-    #     levels = (; levels..., Symbol("level$l") = layer)
-    # end
+    levels = (;)
+    for l in 1:nLevels
+        N = Ns[l]
+        shape = (N, N, N,)
+        layer = if prod(shape) <= nEmbeddings
+            SpatialGrid(out_dims, shape; init_weight, interpfun)
+        else
+            SpatialHash(out_dims, nEmbeddings, shape; init_weight, interpfun)
+        end
+        levels = (; levels..., Symbol("level$l") => layer)
+    end
 
-    levels = NamedTuple(
-        Symbol("Level$l") => SpatialHash(
-            out_dims, nEmbeddings, (Ns[l], Ns[l], Ns[l]);
-            init_weight, interpfun,
-        )
-        for l in 1:nLevels
-    )
-
-    # Chain(;
-    #     branch = BranchLayer(; noop = NoOpLayer(), levels...),
-    #     Vcat = Parallel(vcat, (NoOpLayer() for _ in 1:nLevels+1)...),
-    # )
     Chain(;
-        branch = BranchLayer(; levels...),
-        Vcat = Parallel(vcat, (NoOpLayer() for _ in 1:nLevels)...),
+        grids = BranchLayer(; levels...),
+        vcat  = WrappedFunction(__vcat),
     )
 end
 
-function precompute_indices(xyz, MLH)
-    w1 = (wx, wy, wz)
-    I1 = (I000, I100)
-    (
-        (w1, I1),
-        (w2, I2),
+__vcat(x::NTuple{N,AbstractArray}) where{N} = vcat(x...)
+
+function precompute_MLH(xyz, MLH)
+    ids = ()
+
+    layers = MLH.layers.grids.layers
+
+    for layer in layers
+        bbox = get_boundingbox(xyz, layer.shape, layer.embedding.in_dims, layer.indexfun)
+        ids = (ids..., bbox)
+    end
+
+    NN = Parallel(vcat; layers...)
+    NN = Chain(;
+        grids = Parallel(vcat; layers...),
     )
+
+    ids, NN
 end
 
 #===========================================================#
