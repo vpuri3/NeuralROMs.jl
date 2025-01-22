095c935654a4a3182723e41a700cdbd3d4564e46
Vedant Puri <vedantpuri@gmail.com>
2025-01-22 13:07:00 -0500

    dump MLIR

--- misc/react_demo.jl
+++ misc/react_demo.jl
@@ -4,6 +4,8 @@ using Zygote, Enzyme, Reactant
 using CUDA, LuxCUDA, KernelAbstractions
 using Random, Printf, Optimisers, MLUtils
 
+Lux.Lux.DUMP_REACTANT_HLO_OPT_MODE[] = true
+
 function train(device, adtype)
 
 	N = 10000
@@ -35,7 +37,9 @@ function train(device, adtype)
 		loss = 0
         for batch in DL
             _, loss, _, train_state = Training.single_train_step!(
-                adtype, MSELoss(), batch, train_state)
+                adtype, MSELoss(), batch, train_state;
+				return_gradients = Training.False(),
+			)
         end
     end
 
@@ -46,18 +50,28 @@ end
 # CPU
 ####
 
+train(cpu_device(), AutoZygote())
+train(cpu_device(), AutoEnzyme())
+Reactant.set_default_backend("cpu")
+train(reactant_device(), AutoEnzyme())
+
 @time train(cpu_device(), AutoZygote())
 @time train(cpu_device(), AutoEnzyme())
-Reactant.set_default_backend("cpu")
 @time train(reactant_device(), AutoEnzyme())
+
+println()
 
 ####
 # GPU
 ####
 
+train(gpu_device(), AutoZygote())
+# train(gpu_device(), AutoEnzyme()) # failing
+Reactant.set_default_backend("gpu")
+train(reactant_device(), AutoEnzyme())
+
 @time train(gpu_device(), AutoZygote())
 # @time train(gpu_device(), AutoEnzyme()) # failing
-Reactant.set_default_backend("gpu")
 @time train(reactant_device(), AutoEnzyme())
 
 #====================================================#
